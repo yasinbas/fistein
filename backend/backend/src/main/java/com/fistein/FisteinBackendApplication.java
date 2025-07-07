2d3e13ab89f387423ba3e7533df78bb9ff3b26f8
Yasin <yasinbas1@hotmail.com>
2025-07-07 12:28:22 +0300

    feat: initialize Spring Boot backend

---
+++ backend/backend/src/main/java/com/fistein/FisteinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fistein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisteinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisteinBackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/fistein/FisteinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fistein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisteinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
