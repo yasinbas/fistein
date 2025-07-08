d92a2f6a5e4f36edea1689c9974d6ef68b36cfbf
Yasin <yasinbas1@hotmail.com>
2025-07-08 12:17:04 +0300

    added tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,28 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+/ tailwind.config.js
+module.exports = {
   theme: {
     extend: {
       colors: {
         primary: {
-          50: '#f0f9ff',
+          50:  '#f5faff',
           100: '#e0f2fe',
           200: '#bae6fd',
           300: '#7dd3fc',
           400: '#38bdf8',
           500: '#0ea5e9',
-          600: '#0284c7',
+          600: '#0284c7', // Burada 600 tonu!
           700: '#0369a1',
           800: '#075985',
           900: '#0c4a6e',
         }
       }
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    }
+  }
 }
